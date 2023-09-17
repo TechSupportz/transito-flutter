7181eb371ddc250fba44953df02de22f35009d0f
Nitish <tnitish654@gmail.com>
2023-09-17 12:17:28 +0800

    🐞 Fixed Android builds failing ⚡ Migrated Android build to Kotlin

--- android/app/src/main/java/tnitish/com/transito/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.tnitish.transito;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/tnitish/transito_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tnitish.transito
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
