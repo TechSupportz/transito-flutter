c5753d40e2d58230c6f93a8724a5aa4dce034d4b
Nitish <tnitish654@gmail.com>
2025-07-05 14:03:44 +0800

    ⚡ Update map search loading states

--- lib/widgets/search/search_dialog.dart
+++ lib/widgets/search/search_dialog.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_skeleton_ui/flutter_skeleton_ui.dart';
 import 'package:http/http.dart' as http;
 import 'package:transito/models/api/transito/onemap/onemap_search.dart';
 import 'package:transito/models/secret.dart';
@@ -21,6 +22,10 @@ class _SearchDialogState extends State<SearchDialog> {
   late final FocusNode _searchFocusNode = FocusNode();
 
   Future<OneMapSearch> getSearchResults(String query, int page) async {
+    if (query.isEmpty) {
+      return Future.value(OneMapSearch(totalCount: 0, count: 0, totalPages: 0, page: 1, data: []));
+    }
+
     final response =
         await http.get(Uri.parse('${Secret.API_URL}/onemap/search?query=$query&page=$page'));
 
@@ -68,6 +73,7 @@ class _SearchDialogState extends State<SearchDialog> {
     return Dialog.fullscreen(
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.surfaceContainerHigh,
           title: TextField(
             controller: _textFieldController,
             focusNode: _searchFocusNode,
@@ -93,28 +99,131 @@ class _SearchDialogState extends State<SearchDialog> {
         body: FutureBuilder<OneMapSearch>(
           future: _searchResults,
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
+            if (snapshot.hasData) {
+              OneMapSearch res = snapshot.data!;
+
+              if (res.count == 0 && _textFieldController.text.isEmpty) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "🔍 Start typing to search for a location 📍",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        "Enter a location name or address to find it on the map",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              if (res.count == 0 && snapshot.connectionState == ConnectionState.done) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "🔍 We couldn't find any locations 🤔",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        "Try checking your search for typos or use a different search term",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              return Skeleton(
+                isLoading: snapshot.connectionState == ConnectionState.waiting,
+                skeleton: SkeletonListView(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 32.0, left: 12.0, right: 12.0),
+                  itemBuilder: (context, _) => SkeletonListTile(
+                    titleStyle: SkeletonLineStyle(
+                      height: 24,
+                      padding: const EdgeInsets.only(bottom: 8),
+                    ),
+                    subtitleStyle: SkeletonLineStyle(
+                      height: 32,
+                      randomLength: true,
+                    ),
+                    hasLeading: false,
+                    hasSubtitle: true,
+                    padding: const EdgeInsets.only(bottom: 16),
+                  ),
+                ),
+                child: ListView.builder(
+                  itemCount: res.count,
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 32.0, left: 12.0, right: 12.0),
+                  itemBuilder: (context, index) {
+                    final result = res.data[index];
+                    return ListTile(
+                      title: Text(result.name),
+                      subtitle: Text(result.address),
+                      onTap: () {
+                        // Handle the tap on the result
+                      },
+                    );
+                  },
+                ),
+              );
             } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.data.isEmpty) {
-              return const Center(child: Text('No results found.'));
-            } else {
-              final results = snapshot.data!.data;
-              return ListView.builder(
-                itemCount: results.length,
-                itemBuilder: (context, index) {
-                  final result = results[index];
-                  return ListTile(
-                    title: Text(result.name),
-                    subtitle: Text(result.address),
-                    onTap: () {
-                      // Handle the tap on the result
-                    },
-                  );
-                },
+              debugPrint("<=== ERROR ${snapshot.error} ===>");
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "⚠️ Something went wrong",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      "Please try again later",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               );
             }
+
+            return const Center(child: CircularProgressIndicator(strokeWidth: 3));
           },
         ),
       ),
