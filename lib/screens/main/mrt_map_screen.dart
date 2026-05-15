a19e1e09c4dfed68d4ca616207e6197787aaa3bd
Nitish <tnitish654@gmail.com>
2026-05-15 12:42:25 +0800

    ⚡ Updated MRT Map to include light and dark mode variants

--- lib/screens/main/mrt_map_screen.dart
+++ lib/screens/main/mrt_map_screen.dart
@@ -8,16 +8,19 @@ class MrtMapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: false,
-      appBar: AppBar(
-        title: const Text('MRT Map'),
-      ),
+      appBar: AppBar(title: const Text('MRT Map')),
       // displays a zoomable mrt map (yes that's literally the only thing this whole screen does)
       body: PhotoView(
         maxScale: PhotoViewComputedScale.contained * 7.5,
         minScale: PhotoViewComputedScale.contained,
         initialScale: PhotoViewComputedScale.covered,
         basePosition: Alignment.centerLeft,
-        imageProvider: const AssetImage('assets/images/mrt_map.png'),
+        backgroundDecoration: BoxDecoration(
+          color: Theme.of(context).brightness == .dark ? Colors.black : Colors.white,
+        ),
+        imageProvider: Theme.of(context).brightness == .dark
+            ? AssetImage('assets/images/mrt_maps/mrt-map-dark.webp')
+            : AssetImage('assets/images/mrt_maps/mrt-map-light.webp'),
       ),
     );
   }
